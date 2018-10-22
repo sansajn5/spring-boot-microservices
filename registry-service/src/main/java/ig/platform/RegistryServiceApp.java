1d07959f11d7d81845f77ec86cec10be739a1bda
sansajn <mudrinic5n@outlook.com>
2018-10-22 19:38:33 +0200

    configured eureka

--- registry-service/src/main/java/ig/platform/RegistryServiceApp.java
+++ registry-service/src/main/java/ig/platform/RegistryServiceApp.java
@@ -2,10 +2,10 @@ package ig.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "service" })
+@EnableEurekaServer
 public class RegistryServiceApp
 {
     public static void main(String[] args) {
